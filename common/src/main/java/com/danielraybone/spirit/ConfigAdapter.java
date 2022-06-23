13814c6a1e07163e3b9fa6d87a81e2c8a48f3719
iAvearges <31937542+iAverages@users.noreply.github.com>
2022-06-23 22:07:38 +0100

    Add default for ConfigAdapter interface

--- common/src/main/java/com/danielraybone/spirit/ConfigAdapter.java
+++ common/src/main/java/com/danielraybone/spirit/ConfigAdapter.java
@@ -1,5 +1,7 @@
 package com.danielraybone.spirit;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,24 +9,54 @@ import java.util.Map;
 public interface ConfigAdapter {
     void reload();
 
-    String getString(String path);
+    default String getString(String path) {
+        return this.getString(path, "");
+    }
+
 
     String getString(String path, String def);
 
-    int getInteger(String path);
+    default int getInteger(String path) {
+        return this.getInteger(path, 0);
+    }
 
     int getInteger(String path, int def);
 
-    boolean getBoolean(String path);
+    default boolean getBoolean(String path) {
+        return this.getBoolean(path, false);
+    }
 
     boolean getBoolean(String path, boolean def);
 
-    List<String> getStringList(String path);
+
+    default List<String> getStringList(String path) {
+        return this.getStringList(path, new ArrayList<>());
+    }
 
     List<String> getStringList(String path, List<String> def);
 
-    Map<String, String> getStringMap(String path);
+    default Map<String, String> getStringMap(String path) {
+        return this.getStringMap(path, new HashMap<>());
+    }
 
     Map<String, String> getStringMap(String path, Map<String, String> def);
+
+//    String getString(String path, String def);
+//
+//    int getInteger(String path);
+//
+//    int getInteger(String path, int def);
+//
+//    boolean getBoolean(String path);
+//
+//    boolean getBoolean(String path, boolean def);
+//
+//    List<String> getStringList(String path);
+//
+//    List<String> getStringList(String path, List<String> def);
+//
+//    Map<String, String> getStringMap(String path);
+//
+//    Map<String, String> getStringMap(String path, Map<String, String> def);
 
 }
--- paper/src/main/java/com/danielraybone/spirit/adapter/PaperConfigAdapter.java
+++ paper/src/main/java/com/danielraybone/spirit/adapter/PaperConfigAdapter.java
@@ -5,7 +5,6 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,18 +24,8 @@ public class PaperConfigAdapter implements ConfigAdapter {
     }
 
     @Override
-    public String getString(String path) {
-        return this.getString(path, "");
-    }
-
-    @Override
     public String getString(String path, String def) {
         return this.configuration.getString(path, def);
-    }
-
-    @Override
-    public int getInteger(String path) {
-        return this.getInteger(path, 0);
     }
 
     @Override
@@ -45,29 +34,14 @@ public class PaperConfigAdapter implements ConfigAdapter {
     }
 
     @Override
-    public boolean getBoolean(String path) {
-        return this.getBoolean(path, false);
-    }
-
-    @Override
     public boolean getBoolean(String path, boolean def) {
         return this.configuration.getBoolean(path, def);
-    }
-
-    @Override
-    public List<String> getStringList(String path) {
-        return this.getStringList(path, new ArrayList<>());
     }
 
     @Override
     public List<String> getStringList(String path, List<String> def) {
         List<String> list = this.configuration.getStringList(path);
         return this.configuration.isSet(path) ? list : def;
-    }
-
-    @Override
-    public Map<String, String> getStringMap(String path) {
-        return this.getStringMap(path, new HashMap<>());
     }
 
     @Override
